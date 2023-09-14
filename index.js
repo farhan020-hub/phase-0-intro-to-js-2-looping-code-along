8df17fd4652985a40f85f27b2643f914cc5884bc
AbdiazizSaman <farhanadam394@gmail.com>
2023-09-14 15:56:39 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solutions in this file
+
+
+const names = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(names) {
+    const thanksMessages = [];
+    
+    for (let i = 0; i < names.length; i++) {
+        const message = `Thank you, ${names[i]}, for the wonderful surprise gift!`;
+        thanksMessages.push(message);
+        
+    }
+
+    return thanksMessages;
+}
+
+
+
+
+function countDown(number) {
+    let i = number;
+
+    while (i >= 0) {
+        console.log(i);
+        i--;
+    }
+}
+
+countDown(10);
+
